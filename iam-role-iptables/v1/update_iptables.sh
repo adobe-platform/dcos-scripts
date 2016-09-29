2b0a9b4116e35b186da917dd84317e30ceb49121
Matt Fuller <matthewdf10@gmail.com>
2016-09-29 17:30:19 -0400

    first commit

---
+++ iam-role-iptables/v1/update_iptables.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+EXISTING=$(sudo iptables -t nat -L | grep instance-data.ec2.internal)
+
+# If existing is not empty, simply sleep
+if [[ ! -z $EXISTING ]]; then
+	echo $(date -u) "IPTABLES rule already exists. Sleeping..."
+	sleep infinity
+fi
+
+echo $(date -u) "IPTABLES rule does not exist. Creating..."
+
+export NETWORK="bridge"
+export GATEWAY="$(ifconfig docker0 | grep "inet " | awk -F: '{print $1}' | awk '{print $2}')"
+export INTERFACE="docker0"
+
+sudo iptables -t nat -I PREROUTING -p tcp -d 169.254.169.254 --dport 80 -j DNAT --to-destination "$GATEWAY":8080 -i "$INTERFACE"
+
+sleep infinity
