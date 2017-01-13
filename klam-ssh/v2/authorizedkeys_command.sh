100bf41818ecb49ae87a1c66de9d9547a4d2458e
tseng <tseng@adobe.com>
2017-01-13 09:45:17 -0800

    support for namespace disabled environments

--- klam-ssh/v2/authorizedkeys_command.sh
+++ klam-ssh/v2/authorizedkeys_command.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -x
 
+USER=$1
+SYSDFILE="/etc/systemd/system/docker.service.d/10-enable-namespaces.conf"
+
+# docker start functions
+start_nsdocker ()
+{
+  docker run --privileged --userns=host --rm -e ROLE_NAME=${ROLE_NAME} -e ENCRYPTION_ID=${ENCRYPTION_ID} -e ENCRYPTION_KEY=${ENCRYPTION_KEY} -e KEY_LOCATION_PREFIX=${KEY_LOCATION_PREFIX} ${IMAGE} /usr/lib/klam/getKeys.py ${USER}
+}
+
+start_docker ()
+{
+  docker run --net=host --rm -e ROLE_NAME=${ROLE_NAME} -e ENCRYPTION_ID=${ENCRYPTION_ID} -e ENCRYPTION_KEY=${ENCRYPTION_KEY} -e KEY_LOCATION_PREFIX=${KEY_LOCATION_PREFIX} ${IMAGE} /usr/lib/klam/getKeys.py ${USER}
+}
+
+
 # Source the etcd
 if [ -f /opt/klam/environment ]; then
   source /opt/klam/environment;
@@ -11,14 +26,22 @@ if grep "pam_mkhomedir" /etc/pam.d/system-login; then
   echo "PAM userdir"
 else
   echo "create userdir"
-  mkdir -p /home/$1 > /dev/null
-  chown -R $1. /home/$1 > /dev/null
+  mkdir -p /home/${USER} > /dev/null
+  chown -R ${USER}. /home/${USER} > /dev/null
 fi
 
 echo "adding user to docker group"
-gpasswd -a $1 docker
+gpasswd -a ${USER} docker
 
-echo "Running authorizedkeys_command for $1" | systemd-cat -p info -t klam-ssh
+echo "Running authorizedkeys_command for ${USER}" | systemd-cat -p info -t klam-ssh
 
-docker run --privileged --userns=host --rm -e ROLE_NAME=${ROLE_NAME} -e ENCRYPTION_ID=${ENCRYPTION_ID} -e ENCRYPTION_KEY=${ENCRYPTION_KEY} -e KEY_LOCATION_PREFIX=${KEY_LOCATION_PREFIX} ${IMAGE} /usr/lib/klam/getKeys.py $1
+if [ -a ${SYSDFILE} ]; then
+  if grep "userns-remap=default" ${SYSDFILE}; then
+    start_nsdocker
+  else
+    start_docker
+  fi
+else
+  start_docker
+fi
 exit 0
