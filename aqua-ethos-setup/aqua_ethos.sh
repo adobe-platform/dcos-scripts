02890f6e1db312857163dad9f8238207d8ac82b7
Matt Fuller <matthewdf10@gmail.com>
2017-01-23 12:07:19 -0500

    adding logging function

--- aqua-ethos-setup/aqua_ethos.sh
+++ aqua-ethos-setup/aqua_ethos.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function log {
+	echo $(date -u) "$1" >> $DIR/aqua_ethos.log
+}
+
+log "CRED_DIR set to $CRED_DIR"
+log "WEB_URL set to $WEB_URL"
+log "HEADER set to $HEADER"
+
+if [[ ! -z "$PASSWORD" ]]; then
+	log "PASSWORD set to ******"
+fi
+
 # Wait for web ui to be active
 WEB_ACTIVE=$(curl --silent $WEB_URL)
 
 while [[ -z $WEB_ACTIVE ]]; do
-  echo "Waiting for web UI to become active"
+  log "Waiting for web UI to become active"
   WEB_ACTIVE=$(curl --silent $WEB_URL)
   sleep 5;
 done
@@ -14,7 +28,7 @@ TOKEN_RESP=$(curl --silent "$WEB_URL/v1/login" -H 'Content-Type: application/jso
 TOKEN=$(echo $TOKEN_RESP | jq -r .token)
 
 if [[ -z $TOKEN ]]; then
-	echo "Unable to log in using user/password"
+	log "Unable to log in using user/password"
 	exit 1
 fi
 
@@ -22,7 +36,7 @@ fi
 EXISTING_RULE=$(curl --write-out %{http_code} --silent --output /dev/null -H "$HEADER: Bearer $TOKEN" $WEB_URL/v1/adminrules/core-user-rule)
 
 if [[ "$EXISTING_RULE" == "200" ]]; then
-	echo "core-user-rule exists..."
+	log "core-user-rule exists..."
 else
 	curl --silent -H "Content-Type: application/json" -H "$HEADER: Bearer $TOKEN" -X POST -d '{"name":"core-user-rule","description": "Core User is Admin of all containers","role":"administrator","resources":{"containers":["*"],"images":["*"],"volumes":["*"],"networks":["*"]},"accessors":{"users":["core"]}}' $WEB_URL/v1/adminrules
 fi
@@ -30,7 +44,7 @@ fi
 EXISTING_PROFILE=$(curl --write-out %{http_code} --silent --output /dev/null -H "$HEADER: Bearer $TOKEN" $WEB_URL/v1/securityprofiles/Ethos)
 
 if [[ "$EXISTING_PROFILE" == "200" ]]; then
-	echo "Ethos profile exists..."
+	log "Ethos profile exists..."
 else
 	curl --silent -H "Accept: application/json" -H "Content-type: application/json" -H "$HEADER: Bearer $TOKEN" -X POST -d '{"name": "Ethos", "type": "security.profile", "description": "Ethos Default RunTime Profile", "encrypt_all_envs": true}' $WEB_URL/v1/securityprofiles
 fi
@@ -55,7 +69,7 @@ curl --silent -H "$HEADER: Bearer $TOKEN" -X PUT -d @$CRED_DIR/threat_mitigation
 sudo rm $CRED_DIR/*
 
 while [[ "$EXISTING_PROFILE" == "200" && "$EXISTING_RULE" == "200" ]]; do
-	echo "Profile and rule are still active..."
+	log "Profile and rule are still active..."
 	TOKEN_RESP=$(curl --silent "$WEB_URL/v1/login" -H 'Content-Type: application/json' --data-binary '{"id":"administrator","password":"'$PASSWORD'"}')
 	TOKEN=$(echo $TOKEN_RESP | jq -r .token)
 
@@ -69,5 +83,5 @@ while [[ "$EXISTING_PROFILE" == "200" && "$EXISTING_RULE" == "200" ]]; do
 	sleep 60
 done
 
-echo "Profile ($EXISTING_PROFILE) or rule ($EXISTING_RULE) could not be found in Aqua, restarting to ensure compliance..."
+log "Profile ($EXISTING_PROFILE) or rule ($EXISTING_RULE) could not be found in Aqua, restarting to ensure compliance..."
 exit 1
