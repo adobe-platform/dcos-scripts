0622004741d50add7a743424a46885b9eddb73c1
Matt Fuller <matthewdf10@gmail.com>
2017-05-01 10:50:08 -0400

    logging response from web

--- aqua-ethos-setup/aqua_ethos.sh
+++ aqua-ethos-setup/aqua_ethos.sh
@@ -48,6 +48,7 @@ function waitForWeb {
 
 	while [[ -z $WEB_ACTIVE ]]; do
 	  log "Waiting for web UI to become active"
+	  log "$WEB_ACTIVE"
 	  WEB_ACTIVE=$(curl --silent $WEB_URL)
 	  sleep 5;
 	done
