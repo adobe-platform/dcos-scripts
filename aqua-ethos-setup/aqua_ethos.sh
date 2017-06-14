bdf5244d2b9381acb733e7a7729ce3f987e091ae
anshu pande <anshu@adobe.com>
2017-06-14 11:09:17 -0400

    testing

--- aqua-ethos-setup/aqua_ethos.sh
+++ aqua-ethos-setup/aqua_ethos.sh
@@ -175,14 +175,15 @@ function replaceConfigs {
 		mv $CONFIG_FILE.bak $CONFIG_FILE
 	fi
 
-	if [[ ! -z "$DELETE_DOCKER_HUB" ]]; then
- 		cat $CONFIG_FILE | jq '.policies.image_assurance[0].allow_images_with_prefixes |= .+ ["adobeplatform"]' > $CONFIG_FILE.bak
-		mv $CONFIG_FILE.bak $CONFIG_FILE
-		cat $CONFIG_FILE | jq '.policies.image_assurance[0].allow_images_with_prefixes |= .+ ["behance"]' > $CONFIG_FILE.bak
-		mv $CONFIG_FILE.bak $CONFIG_FILE
- 	else
- 		# Remove the Docker Hub section
- 		curl --silent -H "Content-Type: application/json" -H "$HEADER: Bearer $TOKEN" -X DELETE $WEB_URL/registries/Docker%20Hub
+# Add dockerHub Prefic
+cat $CONFIG_FILE | jq '.policies.image_assurance[0].allow_images_with_prefixes |= .+ ["adobeplatform"] + ["behance"]' > $CONFIG_FILE.bak
+mv $CONFIG_FILE.bak $CONFIG_FILE
+
+	if [[ "$DELETE_DOCKER_HUB" == true ]]; then
+		# Remove the Docker Hub section
+		curl --silent -H "Content-Type: application/json" -H "$HEADER: Bearer $TOKEN" -X DELETE $WEB_URL/registries/Docker%20Hub
+	else
+		echo "Docker HUB is required"
 	fi
 
 	# Update the encryption mode
