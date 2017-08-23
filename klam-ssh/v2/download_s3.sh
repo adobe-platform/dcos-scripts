b3ea5e18a389fb8c75bd02a143e5b092d06ad316
tseng <tseng@adobe.com>
2017-08-22 18:01:38 -0700

    namespace net=host

--- klam-ssh/v2/download_s3.sh
+++ klam-ssh/v2/download_s3.sh
@@ -6,7 +6,7 @@ SYSDFILE="/etc/systemd/system/docker.service.d/*namespaces*.conf"
 # docker start functions
 start_nsdocker ()
 {
-  docker run --privileged --userns=host --rm -e ROLE_NAME=${ROLE_NAME} -e ENCRYPTION_ID=${ENCRYPTION_ID} -e ENCRYPTION_KEY=${ENCRYPTION_KEY} -e KEY_LOCATION_PREFIX=${KEY_LOCATION_PREFIX} ${IMAGE} /usr/lib/klam/downloadS3.py
+  docker run --net=host --privileged --userns=host --rm -e ROLE_NAME=${ROLE_NAME} -e ENCRYPTION_ID=${ENCRYPTION_ID} -e ENCRYPTION_KEY=${ENCRYPTION_KEY} -e KEY_LOCATION_PREFIX=${KEY_LOCATION_PREFIX} ${IMAGE} /usr/lib/klam/downloadS3.py
 }
 
 start_docker ()
