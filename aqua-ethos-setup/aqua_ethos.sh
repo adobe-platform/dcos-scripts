d3a09fb0783f1b53fb058058cfdfacc471837fa6
Sandeep Sharma <13536626+sandeephello@users.noreply.github.com>
2018-01-31 22:51:40 +0530

    modified config

--- aqua-ethos-setup/aqua_ethos.sh
+++ aqua-ethos-setup/aqua_ethos.sh
@@ -12,7 +12,6 @@ function setup {
 	if [[ -z "$HC_DIR" ]]; then log "HC_DIR environment variable required. Exiting..." && exit 1; fi
 	if [[ -z "$PASSWORD" ]]; then log "PASSWORD environment variable required. Exiting..." && exit 1; fi
 	if [[ -z "$FD_PASSWORD" ]]; then log "FD_PASSWORD environment variable required. Exiting..." && exit 1; fi
-	if [[ -z "$DATADOG_PASSWORD" ]]; then log "DATADOG_PASSWORD environment variable required. Exiting..." && exit 1; fi
 	if [[ -z "$AUDITOR_PASSWORD" ]]; then log "AUDITOR_PASSWORD environment variable required. Exiting..." && exit 1; fi
 	if [[ -z "$SCANNER_PASSWORD" ]]; then log "SCANNER_PASSWORD environment variable required. Exiting..." && exit 1; fi
 	if [[ -z "$ARTIFACTORY_URL" ]]; then log "ARTIFACTORY_URL environment variable required. Exiting..." && exit 1; fi
@@ -67,7 +66,7 @@ function setup {
 	log "HEADER set to $HEADER"
 	log "PASSWORD set to ******"
 	log "FD_PASSWORD set to ******"
-	log "DATADOG_PASSWORD set to ******"
+	if [[ ! -z "$DATADOG_PASSWORD" ]]; then log "DATADOG_PASSWORD set to ******"; fi
 	log "AUDITOR_PASSWORD set to ******"
 	log "SCANNER_PASSWORD set to ******"
 	log "ARTIFACTORY_URL set to $ARTIFACTORY_URL"
@@ -292,7 +291,9 @@ fi
 
 }
 
-createUser "datadog" "datadog" "Datadog" "$DATADOG_PASSWORD" "administrator"
+if [[ ! -z "$DATADOG_PASSWORD" ]]; then
+	createUser "datadog" "datadog" "Datadog" "$DATADOG_PASSWORD" "administrator"
+fi
 createUser "auditor" "auditor" "Auditor" "$AUDITOR_PASSWORD" "auditor"
 createUser "flight-director" "flight-director" "FlightDirector" "$FD_PASSWORD" "administrator"
 createUser "scanner" "scanner" "Scanner" "$SCANNER_PASSWORD" "scanner"
